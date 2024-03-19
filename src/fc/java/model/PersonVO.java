31ed21f81bc3e257e09d09b43f738ec6374c6cfb
Jung-won-seok <ukksj0621@naver.com>
2024-03-19 18:02:17 +0900

    생성자, 오버로딩

--- src/fc/java/model/PersonVO.java
+++ src/fc/java/model/PersonVO.java
@@ -4,6 +4,23 @@ public class PersonVO {
     private String name;
     private int age;
     private String phone;
+    //생략된 생성자 메서드가 있다. → 기본생성자(default Constructor)
+    public PersonVO(){
+        //객체를 생성하는 코드는 내부에서 만들어진다.
+        //객체의 초기화를 한다.
+        this.name = "홍길동";
+        this.age = 50;
+        this.phone = "010-1111-1111";
+    }
+    // 생성자의 오버로딩
+    public PersonVO(String name, int age, String phone){
+        this.name = name;
+        this.age = age;
+        this.phone = phone;
+    }
+
+
+
     // setter method
     public void setName(String name){
         this.name=name;
