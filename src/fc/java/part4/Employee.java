067e03cfc66b8f73016a5efacb5dff3dbcd5f611
Jung-won-seok <ukksj0621@naver.com>
2024-03-25 22:13:33 +0900

    commit

--- src/fc/java/part4/Employee.java
+++ src/fc/java/part4/Employee.java
@@ -1,14 +1,22 @@
 package fc.java.part4;
 // 사원(VO, DTO)
 public class Employee extends Object{ // object 클래스 -> 최상위 클래스 상속
-    protected String name;
-    protected int age;
-    protected String phone;
-    protected String empDate;
-    protected String dept;
-    protected boolean marriage;
+    private String name;
+    private int age;
+    private String phone;
+    private String empDate;
+    private String dept;
+    private boolean marriage;
     public Employee(){
         super(); // 상위 클래스의 생성자를 호출 -> new Object();
+    }
+    public Employee(String name, int age, String phone, String empDate, String dept, boolean marriage) {
+        this.name = name;
+        this.age = age;
+        this.phone = phone;
+        this.empDate = empDate;
+        this.dept = dept;
+        this.marriage = marriage;
     }
     //toString()
 
